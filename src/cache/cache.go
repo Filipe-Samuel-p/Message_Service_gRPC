75cbce26a79adedaf9200632b7b70e2cfce866ed
Filipe-Samuel-p <felipepires.p125@gmail.com>
2026-04-10 23:40:48 -0300

    feat: redis ok

--- src/cache/cache.go
+++ src/cache/cache.go
@@ -9,10 +9,27 @@ import (
 )
 
 type RedisClient struct {
-	rdb *redis.Conn
+	client *redis.Client
 }
 
-func (rdb *RedisClient) SetUserOnline(userID uuid.UUID) error {
-	ctx := context.Background()
-	return rdb.rdb.Set(ctx, "online:"+userID.String(), "online", 60*time.Second).Err()
+func NewRedisClient(client *redis.Client) *RedisClient {
+	return &RedisClient{
+		client: client,
+	}
+}
+
+func (r *RedisClient) SetUserOnline(ctx context.Context, userID uuid.UUID) error {
+	return r.client.Set(ctx, "online:"+userID.String(), "true", 60*time.Second).Err()
+}
+
+func (r *RedisClient) KeepAlive(ctx context.Context, userID uuid.UUID) error {
+	return r.client.Expire(ctx, "online:"+userID.String(), 60*time.Second).Err()
+}
+
+func (r *RedisClient) IsOnline(ctx context.Context, userID uuid.UUID) (bool, error) {
+	val, err := r.client.Exists(ctx, "online:"+userID.String()).Result()
+	if err != nil {
+		return false, err
+	}
+	return val > 0, nil
 }
